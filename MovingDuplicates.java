8fb4976bec3559a232e0aba9440c9f9a3985d647
Karnam Bhakar Rao <rao03114@stthomas.edu>
2017-05-23 09:52:48 -0500

    Create MovingDuplicates.java

---
+++ MovingDuplicates.java
@@ -0,0 +1,36 @@
+package hadooppackage;
+
+import java.util.ArrayList;
+
+public class MovingDuplicates {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*ArrayList arrlist = new ArrayList();
+		arrlist.add(2);
+		arrlist.add(3);
+		arrlist.add(1);
+		arrlist.add(1);
+		arrlist.add(2);
+		arrlist.add(3); */
+		int[] array1 = {1,2,3,4,5,6,7,1,1,2,3,4};
+
+
+		
+		System.out.println(array1[0]);
+
+		
+		
+		for(int i=0;i < array1.length;i++)
+		{
+			if(array1.equals(array1))
+				
+			{
+				
+			}
+		}
+		 
+
+	}
+
+}
